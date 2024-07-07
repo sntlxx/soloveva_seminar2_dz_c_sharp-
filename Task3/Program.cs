9d341dd62978825d5556dbcf37ef1b2823cbb8c2
Natalie <sonatalex@gmail.com>
2024-07-07 18:58:16 +0300

    task3 is done

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("Введите число от 10 до 99");
+int number = Convert.ToInt32(Console.ReadLine ());
+if (number >= 10 && number <= 99)
+{
+int first = number / 10;
+int second = number % 10;
+if (first > second)
+{
+    System.Console.WriteLine($"{first} является большим");
+}
+else if (second > first)
+{
+    System.Console.WriteLine($"{second} является большим");
+}
+else if (first == second)
+{
+    System.Console.WriteLine($"{first} и {second} равны друг другу");
+}
+}
+
+
+
+else
+{
+    System.Console.WriteLine("Введено неверное число!");
+}
