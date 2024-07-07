b02f08c86457d353134acc5799175fec50561255
Natalie <sonatalex@gmail.com>
2024-07-07 19:07:58 +0300

    task 4 is done

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+ Console.WriteLine("Вы ввели некорректное число.");
+}
+else
+{
+ string numberString = number.ToString();
+ for (int i = 0; i < numberString.Length; i++)
+ {
+ if (i > 0)
+ {
+ Console.Write(", ");
+ }
+ Console.Write(numberString[i]);
+ }
+}
