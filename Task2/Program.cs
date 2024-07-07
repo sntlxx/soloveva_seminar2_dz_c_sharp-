773fd557be5eb9e66e7c1d7cf2397b7a8463009e
Natalie <sonatalex@gmail.com>
2024-07-07 18:37:18 +0300

    task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿System.Console.WriteLine("Введите координату X");
+int x = Convert.ToInt32(Console.ReadLine ());
+System.Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine ());
+
+if(x > 0 && y > 0)
+{
+    System.Console.Write("Плоскость I");
+}
+else
+{
+    if (x > 0 && y < 0)
+    {
+        System.Console.Write("Плоскость II");
+    }
+    else
+    {
+        if (x < 0 && y < 0)
+        {
+            System.Console.Write("Плоскость III");
+        }
+        else
+        {
+            if (x > 0 && y < 0)
+            {
+                System.Console.Write("Плоскость IV");
+            }
+            else 
+            {
+                System.Console.WriteLine("Точка не принадлежит ни одной из нужных плоскостей");
+            }
+        }
+        
+    }
+}
+
